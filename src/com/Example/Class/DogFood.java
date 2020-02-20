259a192d846d94287b9759e5e2ce88c55f76fd07
ioanarebrisorean <ioana.rebrisorean@yahoo.com>
2020-02-20 10:29:31 +0200

    none

--- src/com/Example/Class/DogFood.java
+++ src/com/Example/Class/DogFood.java
@@ -1,4 +1,14 @@
 package com.Example.Class;
 
 public class DogFood extends Food {
+
+    private String foodType;
+
+    public String getFoodType() {
+        return foodType;
+    }
+
+    public void setFoodType(String foodType) {
+        this.foodType = foodType;
+    }
 }
